da98fb83eea90ca7a1478e2b3a96f0b8e28d5f62
KhasanMeliev <khasanmeliev2@gmail.com>
2026-01-04 12:38:29 +0500

    new problem #1390

--- index.py
+++ index.py
@@ -1,7 +1,21 @@
-from collections import Counter
-def func(nums):
-    d = Counter(nums)
-    n=max(d, key=d.get)
-    return n
+def func(arr):
+    res = 0
+    for i in arr:
+        divisor=0
+        subRes = 0
+        for j in range(2, int(i**0.5)+1):
+            if i%j==0:
+                if i//j==j:
+                    divisor=10
+                divisor+=2
+                subRes=(j+i//j)+1+i
+            if divisor>2:
+                break
+            
+        if divisor==2:
+            res+=subRes
+        
 
-print(func([1,2,3,3]))
+    return res
+
+print(func([21,4,7,16])) #32
--- solved.py
+++ solved.py
@@ -29,9 +29,34 @@
 #             d[i] = 1
 
 
-#----------------------------
+#-----------------------------------------------------
 # 3794. Reverse String Prefix
 
 # class Solution:
 #     def reversePrefix(self, s: str, k: int) -> str:
 #         return s[:k][::-1]+s[k:]
+
+#-----------------------------------------------------
+
+# 1390. Four Divisors
+# def func(arr):
+#     res = 0
+#     for i in arr:
+#         divisor=0
+#         subRes = 0
+#         for j in range(2, int(i**0.5)+1):
+#             if i%j==0:
+#                 if i//j==j:
+#                     divisor=10
+#                 divisor+=2
+#                 subRes=(j+i//j)+1+i
+#             if divisor>2:
+#                 break
+            
+#         if divisor==2:
+#             res+=subRes
+        
+
+#     return res
+
+# print(func([21,4,7,16])) #32
