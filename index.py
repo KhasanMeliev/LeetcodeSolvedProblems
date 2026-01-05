2016164c815101f9bd530f168f7b3fdeafb48335
KhasanMeliev <khasanmeliev2@gmail.com>
2026-01-05 11:37:09 +0500

    #1975

--- index.py
+++ index.py
@@ -1,21 +1,17 @@
 def func(arr):
     res = 0
+    cnt = 0
+    mini = float("infinity")
+
     for i in arr:
-        divisor=0
-        subRes = 0
-        for j in range(2, int(i**0.5)+1):
-            if i%j==0:
-                if i//j==j:
-                    divisor=10
-                divisor+=2
-                subRes=(j+i//j)+1+i
-            if divisor>2:
-                break
-            
-        if divisor==2:
-            res+=subRes
-        
+        for j in i:
+            res += abs(j)
+            mini = min(mini, abs(j))
+            if j <= 0:
+                cnt += 1
 
-    return res
+    return res if cnt % 2 == 0 else res - (mini * 2)
 
-print(func([21,4,7,16])) #32
+
+print(func([[-1, 0, -1], [-2, 1, 3], [3, 2, 2]]))
+print(func([[2, 9, 3], [5, 4, -4], [1, 7, 1]]))
--- solved.py
+++ solved.py
@@ -60,3 +60,24 @@
 #     return res
 
 # print(func([21,4,7,16])) #32
+
+#------------------------------------------------------------------
+
+# 1975. Maximum Matrix Sum
+# def func(arr):
+#     res = 0
+#     cnt = 0
+#     mini = float("infinity")
+
+#     for i in arr:
+#         for j in i:
+#             res += abs(j)
+#             mini = min(mini, abs(j))
+#             if j <= 0:
+#                 cnt += 1
+
+#     return res if cnt % 2 == 0 else res - (mini * 2)
+
+
+# print(func([[-1, 0, -1], [-2, 1, 3], [3, 2, 2]]))
+# print(func([[2, 9, 3], [5, 4, -4], [1, 7, 1]]))
