c74ccfdff7126a160dd63a51529dc629dc6609bc
KhasanMeliev <khasanmeliev2@gmail.com>
2026-01-06 13:22:29 +0500

    #1161

--- index.py
+++ index.py
@@ -1,17 +1,55 @@
-def func(arr):
+from collections import deque
+
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+def func(root):
+    if not root:
+        return
+
+    queue = deque([(root)])
     res = 0
-    cnt = 0
-    mini = float("infinity")
+    maxi = root.val
+    level=1
+    while queue:
+        curr_sum = 0
+        for _ in range(len(queue)):
+            node = queue.popleft()
+            curr_sum += node.val
+            
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
 
-    for i in arr:
-        for j in i:
-            res += abs(j)
-            mini = min(mini, abs(j))
-            if j <= 0:
-                cnt += 1
+        if maxi < curr_sum:
+            maxi = curr_sum
+            res = level
 
-    return res if cnt % 2 == 0 else res - (mini * 2)
+        level += 1
+
+    return res
 
 
-print(func([[-1, 0, -1], [-2, 1, 3], [3, 2, 2]]))
-print(func([[2, 9, 3], [5, 4, -4], [1, 7, 1]]))
+# a = Node(1)
+# a.left = Node(7)
+# a.right = Node(0)
+# b = a.left
+# b.left = Node(7)
+# b.right = Node(-8)
+
+a = Node(-100)
+a.left = Node(-200)
+a.right = Node(-300)
+b = a.left
+c = a.right
+b.left = Node(-20)
+b.right = Node(-5)
+c.left = Node(-10)
+
+print(func(a))
--- solved.py
+++ solved.py
@@ -81,3 +81,63 @@
 
 # print(func([[-1, 0, -1], [-2, 1, 3], [3, 2, 2]]))
 # print(func([[2, 9, 3], [5, 4, -4], [1, 7, 1]]))
+
+
+#---------------------------------------------------------------
+
+# 1161. Maximum Level Sum of a Binary Tree
+# from collections import deque
+
+
+# class Node:
+#     def __init__(self, val):
+#         self.val = val
+#         self.left = None
+#         self.right = None
+
+
+# def func(root):
+#     if not root:
+#         return
+
+#     queue = deque([(root)])
+#     res = 0
+#     maxi = root.val
+#     level=1
+#     while queue:
+#         curr_sum = 0
+#         for _ in range(len(queue)):
+#             node = queue.popleft()
+#             curr_sum += node.val
+            
+#             if node.left:
+#                 queue.append(node.left)
+#             if node.right:
+#                 queue.append(node.right)
+
+#         if maxi < curr_sum:
+#             maxi = curr_sum
+#             res = level
+
+#         level += 1
+
+#     return res
+
+
+# # a = Node(1)
+# # a.left = Node(7)
+# # a.right = Node(0)
+# # b = a.left
+# # b.left = Node(7)
+# # b.right = Node(-8)
+
+# a = Node(-100)
+# a.left = Node(-200)
+# a.right = Node(-300)
+# b = a.left
+# c = a.right
+# b.left = Node(-20)
+# b.right = Node(-5)
+# c.left = Node(-10)
+
+# print(func(a))
