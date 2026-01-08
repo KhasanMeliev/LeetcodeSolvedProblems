29d9929d2362a02750d332f00571c25c8670b716
KhasanMeliev <khasanmeliev2@gmail.com>
2026-01-08 13:35:45 +0500

    #1458

--- index.py
+++ index.py
@@ -1,44 +1,19 @@
-from collections import deque
+def func(nums1, nums2):
+    n,m=len(nums1), len(nums2)
+    dp = [[0]*(m) for _ in range(n)]
+    
+    for i in range(n):
+        for j in range(m):
+            product = nums1[i]*nums2[j]
+            dp[i][j]=product
 
+            if i>0 and j>0:
+                dp[i][j] = max(dp[i][j], dp[i-1][j-1]+product)
+            if i>0:
+                dp[i][j] = max(dp[i][j], dp[i-1][j])
+            if j>0:
+                dp[i][j] = max(dp[i][j], dp[i][j-1])
+    
+    return dp[i][j]
 
-class Node:
-    def __init__(self, val):
-        self.val = val
-        self.left = None
-        self.right = None
-
-
-def func(root):
-    tree_sums = []
-    def dfs(node):
-        if not node:
-            return 0
-        l_sum = dfs(node.left)
-        r_sum = dfs(node.right)
-        
-        cur_sum = l_sum+r_sum+node.val
-
-        tree_sums.append(cur_sum)
-
-        return cur_sum
-
-    total_sum = dfs(root)
-    max_product = 0
-    for sum_ in tree_sums:
-        max_product = max(max_product, (total_sum-sum_)*sum_)
-
-    return max_product
-
-        
-
-
-a = Node(1)
-a.left = Node(2)
-a.right = Node(3)
-b = a.left
-c = a.right
-b.left = Node(4)
-b.right = Node(5)
-c.left = Node(6)
-
-print(func(a))
+print(func([2,1,-2,5],[3,0,-6]))
--- solved.py
+++ solved.py
@@ -188,3 +188,24 @@
 # c.left = Node(6)
 
 # print(func(a))
+
+#-----------------------------------------------------------------------
+# 1458. Max Dot Product of Two Subsequences
+# def func(nums1, nums2):
+#     n,m=len(nums1), len(nums2)
+#     dp = [[0]*(m) for _ in range(n)]
+    
+#     for i in range(n):
+#         for j in range(m):
+#             product = nums1[i]*nums2[j]
+#             dp[i][j]=product
+
+#             if i>0 and j>0:
+#                 dp[i][j] = max(dp[i][j], dp[i-1][j-1]+product)
+#             if i>0:
+#                 dp[i][j] = max(dp[i][j], dp[i-1][j])
+#             if j>0:
+#                 dp[i][j] = max(dp[i][j], dp[i][j-1])
+#     return dp[i][j]
+
+# print(func([2,1,-2,5],[3,0,-6]))
