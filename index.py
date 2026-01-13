ca81949b6808043c28c0bb463b593cdff0a9e627
KhasanMeliev <khasanmeliev2@gmail.com>
2026-01-13 10:35:53 +0500

    solving...

--- index.py
+++ index.py
@@ -1,19 +1,17 @@
-def func(s):
-    def helper(s, left, right):
-        while left>=0 and right<len(s)-1 and s[left]==s[right]:
-            left-=1
-            right+=1
-        return right-left-1
-    start = 0
-    end = 0
-    for i in range(len(s)):
-        odd = helper(s,i,i)
-        even = helper(s,i,i+1)
-        max_length = max(even, odd)
-        print(max_length)
-        if max_length>end-start:
-            start=i-(max_length-1)//2
-            end=i+max_length//2
-    return s[start:end+1]
+def func(nums):
+    res = 0
+    for i in range(len(nums)):
+        l,r=i,i+1
+        while r<len(nums)-1:
+            if nums[r]-nums[l]==1:
+                l+=1
+                r+=1
+                print(l,r,nums[r], nums[l])
+                res+=1
+            else:
+                break
+    return res
+            
 
-print(func('babad'))
+
+print(func([2, 3, 4, 3, 4]))
