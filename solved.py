11386df7ca22f9ae5618a0ca10d78ea2dfaf8c0f
KhasanMeliev <khasanmeliev2@gmail.com>
2026-01-15 14:24:59 +0500

    solved

--- solved.py
+++ solved.py
@@ -252,3 +252,23 @@
 
 
 # print(func(a))
+
+
+# 2943. Maximize Area of Square Hole in Grid
+# class Solution:
+#     def maximizeSquareHoleArea(
+#         self, n: int, m: int, hBars: List[int], vBars: List[int]
+#     ) -> int:
+#         def maxSpan(bars):
+#             bars.sort()
+#             res = 1
+#             streak = 1
+#             for i in range(1, len(bars)):
+#                 if bars[i] - bars[i - 1] == 1:
+#                     streak += 1
+#                 else:
+#                     streak = 1
+#                 res = max(res, streak)
+#             return res + 1
+
+#         return min(maxSpan(hBars), maxSpan(vBars)) ** 2
