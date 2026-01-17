bad82886d42d8242fa37de0da2af1f621eceaaba
KhasanMeliev <khasanmeliev2@gmail.com>
2026-01-17 10:38:14 +0500

    solving...

--- index.py
+++ index.py
@@ -1,20 +1,11 @@
-def func(s, t, maxCost):
-    n=len(s)
-    cost = []
-    for i in range(n):
-        cost.append(abs(ord(s[i])-ord(t[i])))
+def func(arr):
+    res = set()
+    cur = set()
+    for num in arr:
+        cur = {num | x for x in cur} | {num}
+        res |= cur
+        print(res, cur, num) 
+    return len(res)
 
-    left= 0
-    curr_cost = 0
-    res = 0
-    for right in range(n):
-        curr_cost+=cost[right]
-        while curr_cost>maxCost:
-            curr_cost-=cost[left]
-            left+=1
 
-        res=max(res, right-left+1)
-
-    return res
-
-print(func('abcd', 'bcdf', 3))
+print(func([1, 1, 2]))
