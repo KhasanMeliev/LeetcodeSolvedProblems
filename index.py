65f3825d9f5d3487d553662da727516d0515437b
KhasanMeliev <khasanmeliev2@gmail.com>
2026-01-18 16:44:42 +0500

    solving...

--- index.py
+++ index.py
@@ -1,11 +1,19 @@
-def func(arr):
-    res = set()
-    cur = set()
-    for num in arr:
-        cur = {num | x for x in cur} | {num}
-        res |= cur
-        print(res, cur, num) 
-    return len(res)
+def func(nums,target):
+    nums.sort()
+    coverage = 0
+    added = 0
+    i=0
+    while coverage<target:
+        if i<len(nums) and nums[i]<=coverage+1:
+            coverage+=nums[i]
+            print('kichkina', nums[i], coverage)
+            i+=1
+        else:
+            print(coverage,'+ 1 +', coverage, '---', added) 
+            coverage+=coverage+1
+            added+=1
+    
 
+    return added
 
-print(func([1, 1, 2]))
+print(func([1,3],6))
