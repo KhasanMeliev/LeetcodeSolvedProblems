87f426558dbf3363af9e1a26db4c83c45be19cb8
KhasanMeliev <khasanmeliev2@gmail.com>
2026-01-26 12:21:05 +0500

    new problem solved

--- index.py
+++ index.py
@@ -1,18 +1,17 @@
-def func(nums,target):
-    nums.sort()
-    coverage = 0
-    added = 0
-    i=0
-    while coverage<target:
-        if i<len(nums) and nums[i]<=coverage+1:
-            coverage+=nums[i]
-            print('kichkina', nums[i], coverage)
-            i+=1
-        else:
-            print(coverage,'+ 1 +', coverage, '---', added) 
-            coverage+=coverage+1
-            added+=1
-    
-    return added
+def func(s):
+    n = len(s)
+    obj = {}
+    cnt = 0
+    for i in range(0, len(s)-1, 2):
+        color = s[i]
+        pos = s[i+1]
+        if pos not in obj:
+            obj[pos] = []
+        obj[pos].append(color)
+    for i in obj.values():
+        if 'R' in i and 'G' in i and 'B' in i:
+            cnt += 1
+    return cnt
 
-print(func([1,3],6))
+
+print(func('B0B6G0R6R0R6G9'))
