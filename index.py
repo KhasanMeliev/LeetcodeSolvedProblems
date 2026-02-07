ea20dfc9b2882acbefa60e915b0660b704452397
KhasanMeliev <khasanmeliev2@gmail.com>
2026-02-07 11:22:15 +0500

    added new problem solution

--- index.py
+++ index.py
@@ -1,18 +1,45 @@
-def func(nums, k):
-    positive = []
-    for i in nums:
-        if i >= 0:
-            positive.append(i)
-    k = k % len(positive)
-    x = 0
-    for i in range(len(nums)):
-        if nums[i] >= 0:
-            nums[i] = positive[(x+k) % len(positive)]
-            x += 1
+def func(s):
+    n = len(s)
+    pref = [0]*n
+    suff = [0]*n
+    if s[0] == 'b':
+        pref[0] = 1
+    if s[-1] == 'a':
+        suff[-1] = 1
 
-    return nums
+    for i in range(1, len(s)):
+        pref[i] = pref[i-1]
+        if s[i] == 'b':
+            pref[i] += 1
+    for i in range(len(s)-2, -1, -1):
+        suff[i] = suff[i+1]
+        if s[i] == 'a':
+            suff[i] += 1
+    # return suff
+    res = float('inf')
+    for i in range(len(s)):
+        left = pref[i-1] if i > 0 else 0
+        res = min(res, (left+suff[i]))
+
+    res = min(res, pref[n-1])
+
+    return res
 
 
-print(func([3, 17, 3], 89646))
-# print(func([5, 4, -9, 6], 10))
-# print(func([1, -2, 3, -4], 3))
+print(func('aabbbbaabababbbbaaaaaabbababaaabaabaabbbabbbbabbabbababaabaababbbbaaaaabbabbabaaaabbbabaaaabbaaabbbaabbaaaaabaa'))
+
+
+# def func(s):
+#     res = count = 0
+
+#     for i in s:
+#         if i == 'b':
+#             count += 1
+#         elif count:
+#             res += 1
+#             count -= 1
+
+#     return res
+
+
+# print(func('aabbbbaabababbbbaaaaaabbababaaabaabaabbbabbbbabbabbababaabaababbbbaaaaabbabbabaaaabbbabaaaabbaaabbbaabbaaaaabaa'))
