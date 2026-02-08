09e24838d1bd73acce2ec509d3fb91b58a6e38c0
KhasanMeliev <khasanmeliev2@gmail.com>
2026-02-08 19:46:57 +0500

    added NODE problem

--- index.py
+++ index.py
@@ -1,17 +1,33 @@
-import bisect
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
-def func(arr, n):
-    cur = n + 1
+def isBalanced(root):
+    def dfs(root):
+        if not root:
+            return 0
+        left = dfs(root.left)
+        right = dfs(root.right)
+        if left == -1 or right == -1:
+            return -1
+        if abs(left-right) > 1:
+            return -1
 
-    idx = bisect.bisect_left(arr, cur)
-
-    while idx < len(arr) and arr[idx] == cur:
-        cur += 1
-        idx += 1
-    
-    return cur
+        return max(left, right)+1
+    return dfs(root) != -1
 
 
-print(func([2, 7, 5, 4, 9, 15], 3))
-print(func([2, 7, 5, 9, 15], 9))
+a = Node(3)
+b = Node(9)
+c = Node(20)
+d = Node(15)
+e = Node(7)
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+
+print(isBalanced(a))
