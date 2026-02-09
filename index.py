42590ff9542afcad4a18a4982f0ff1ffe430265e
KhasanMeliev <khasanmeliev2@gmail.com>
2026-02-09 19:31:03 +0500

    solved

--- index.py
+++ index.py
@@ -1,33 +1,10 @@
-class Node:
-    def __init__(self, val, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+def func(n, k):
+    x = (n+1)//2
+    if k <= x:
+        return 2*k-1
+    else:
+        return 2*(k-x)
 
 
-def isBalanced(root):
-    def dfs(root):
-        if not root:
-            return 0
-        left = dfs(root.left)
-        right = dfs(root.right)
-        if left == -1 or right == -1:
-            return -1
-        if abs(left-right) > 1:
-            return -1
-
-        return max(left, right)+1
-    return dfs(root) != -1
-
-
-a = Node(3)
-b = Node(9)
-c = Node(20)
-d = Node(15)
-e = Node(7)
-a.left = b
-a.right = c
-b.left = d
-b.right = e
-
-print(isBalanced(a))
+n, k = map(int, input().split())
+print(func(n, k))
