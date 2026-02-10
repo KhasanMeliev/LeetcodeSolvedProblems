fada6fe051bec984c773d64ee0b07142b72ca22b
KhasanMeliev <khasanmeliev2@gmail.com>
2026-02-10 13:53:04 +0500

    node problems

--- index.py
+++ index.py
@@ -1,10 +1,32 @@
-def func(n, k):
-    x = (n+1)//2
-    if k <= x:
-        return 2*k-1
-    else:
-        return 2*(k-x)
+from collections import deque
 
 
-n, k = map(int, input().split())
-print(func(n, k))
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def func(root):
+    res = []
+
+    def dfs(node,level):
+        if not node:
+            return 
+        if level==len(res):
+            res.append([])
+
+
+a = Node(3)
+b = Node(9)
+c = Node(20)
+d = Node(15)
+e = Node(7)
+
+a.left = b
+a.right = c
+c.left = d
+c.right = e
+
+print(func(a))
