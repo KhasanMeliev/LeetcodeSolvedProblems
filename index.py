729828ec391f232d23a7d91acf8f5d7565553a7f
KhasanMeliev <khasanmeliev2@gmail.com>
2026-02-13 14:17:21 +0500

    solved problems

--- index.py
+++ index.py
@@ -1,24 +1,18 @@
-def func(s1, s2):
-    n = len(s1)
-    m = len(s2)
-    dp = [[1]*(m+1) for _ in range(n+1)]
+def func(n):
+    if int(n**(1/2)) == n**(1/2):
+        return 1
+    while not n % 4:
+        n //= 4
+    if n % 8 == 7:
+        return 4
 
-    for i in range(n+1):
-        for j in range(m+1):
-            if i == 0:
-                dp[i][j] = j
-            elif j == 0:
-                dp[i][j] = i
-            else:
-                if s1[i-1] == s2[j-1]:
-                    dp[i][j] = dp[i-1][j-1]
-                else:
-                    dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1])+1
+    i = 1
+    while i*i < n:
+        if (n-i*i)**(1/2) == int((n-i*i)**(1/2)):
+            return 2
+        i += 1
 
-    for i in dp:
-        print(i)
-
-    return dp[-1][-1]
+    return 3
 
 
-print(func('horse', 'ros'))
+print(func(1103272344472637627))
