9fc1b71a1cc02d7f99d5d643837ac769efc12659
KhasanMeliev <khasanmeliev2@gmail.com>
2026-02-13 20:54:47 +0500

    solved problems

--- index.py
+++ index.py
@@ -1,18 +1,60 @@
-def func(n):
-    if int(n**(1/2)) == n**(1/2):
-        return 1
-    while not n % 4:
-        n //= 4
-    if n % 8 == 7:
-        return 4
+def func(s):
+    n = len(s)
+    if n == 0:
+        return 0
 
-    i = 1
-    while i*i < n:
-        if (n-i*i)**(1/2) == int((n-i*i)**(1/2)):
-            return 2
-        i += 1
+    ans = 1
+    length = 1
+    for i in range(n-1):
+        if s[i] == s[i+1]:
+            length += 1
+        else:
+            ans = max(ans, length)
+            length = 1
 
-    return 3
+    ans = max(ans, length)
+
+    ab = {}
+    bc = {}
+    ca = {}
+    abc = {}
+
+    abc[(0, 0)] = -1
+    ab[(0, 0)] = -1
+    bc[(0, 0)] = -1
+    ca[(0, 0)] = -1
+
+    cnt = [0, 0, 0]
+
+    for i, c in enumerate(s):
+        cnt[ord(c)-ord('a')] += 1
+        a, b, c = cnt
+
+        key = (b-a, c-a)
+        if key in abc:
+            ans = max(ans, i-abc[key])
+        else:
+            abc[key] = i
+
+        key = (a-b, c)
+        if key in ab:
+            ans = max(ans, i-ab[key])
+        else:
+            ab[key] = i
+
+        key = (b-c, a)
+        if key in bc:
+            ans = max(ans, i-bc[key])
+        else:
+            bc[key] = i
+
+        key = (c-a, b)
+        if key in ca:
+            ans = max(ans, i-ca[key])
+        else:
+            ca[key] = i
+
+    return ans
 
 
-print(func(1103272344472637627))
+print(func('abbac'))
