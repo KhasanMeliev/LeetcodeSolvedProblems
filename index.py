025ce6d3863d87274db510112a122ba99d0ef964
KhasanMeliev <khasanmeliev2@gmail.com>
2026-02-17 13:31:52 +0500

    solved

--- index.py
+++ index.py
@@ -1,25 +1,19 @@
-def func(points):
-    rows, cols = len(points), len(points[0])
-    row = points[0]
-
-    for r in range(1, rows):
-        next_row = points[r].copy()
-        left, right = [0]*cols, [0]*cols
-
-        left[0] = row[0]
-        for c in range(1, cols):
-            left[c] = max(row[c], left[c-1]-1)
-
-        right[cols-1] = row[cols-1]
-        for c in range(cols-2, -1, -1):
-            right[c] = max(row[c], right[c+1]-1)
-
-        for c in range(cols):
-            next_row[c] += max(left[c], right[c])
-
-        row = next_row
-
-    return row
+from collections import Counter
 
 
-print(func([[1, 2, 3], [1, 5, 1], [3, 1, 1]]))
+def func(words1, words2):
+    maxFreq = {i: 0 for i in 'abcdefghijklmnopqrstuvwxyz'}
+    for i in words2:
+        for j in i:
+            x = i.count(j)
+            if maxFreq[j] < x:
+                maxFreq[j] = x
+    res = []
+    for i in words1:
+        x = Counter(i)
+        if all(x[j] >= maxFreq[j] for j in maxFreq if maxFreq[j] > 0):
+            res.append(i)
+    return res
+
+
+print(func(["amazon", "apple", "facebook", "google", "leetcode"], ["e", "o"]))
